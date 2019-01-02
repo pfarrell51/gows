9006ac9f48aaf6bd93ff1daef03f09ea12cf8b4d
Pat Farrell <pat22043@gmail.com>
2019-01-02 02:26:22 -0500

    early work to have XML tag parsting

--- src/github.com/pfarrell51/cmd/gpxcvs.go
+++ src/github.com/pfarrell51/cmd/gpxcvs.go
@@ -22,20 +22,33 @@ func main() {
 		if ln < 20 {
 			fmt.Printf("B %d %s\n", ln, line)
 		}
+		part := strings.Split(line, "<")
+		for i := 0; i < len(part); i++ {
+			fmt.Printf("%d %s\n", i, part[i])
+		}
+		switch {
 
-		if strings.HasPrefix(line, "<trkpt") {
+		case strings.HasPrefix(line, "<trkpt"):
 			fmt.Printf("C %d %s\n", ln, line)
-		} else if strings.HasPrefix(line, "<speed") {
+
+		case strings.HasPrefix(line, "<speed"):
 			fmt.Printf("B %d %s\n", ln, line)
-			s := strings.Index(line, ">")+1
+			s := strings.Index(line, ">") + 1
 			sp := strings.Index(line[s:], "<")
 			val := line[s : s+sp]
-			fmt.Printf("C %d %d %d %d %s\n", ln, s, sp, s+sp, val)
 			fmt.Printf("%s\n", val)
+		case strings.HasPrefix(line, "<time"):
+			fmt.Printf("C %d %s\n", ln, line)
+
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// parse xml tag and return parts
+func getXmlParts(line []string) int {
+	return 0
 }
