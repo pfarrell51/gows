5d7a7d5e01e69aeabefb81888c25157becdc2238
Pat Farrell <pat22043@gmail.com>
2019-01-22 00:02:28 -0500

    add prod and sum

--- src/github.com/pfarrell51/cmd/quaternion.go
+++ src/github.com/pfarrell51/cmd/quaternion.go
@@ -3,11 +3,17 @@
 package quaternion
 
 import (
+	"fmt"
 	"math"
 )
 
 type Quaternion struct {
 	a, b, c, d float64
+}
+
+func (q Quaternion) String() string {
+	return fmt.Sprintf("[%g, %gi, %gj, %gk]",
+		q.a, q.b, q.c, q.d)
 }
 
 // The magnitude or length of a Quaternion a + i b + j c+k d is sqrt ( a2 + b2+c2+ d2)
@@ -20,14 +26,11 @@ func (q Quaternion) Magnitude() float64 {
 }
 
 // Norm = || q || = sqrt( q * conj(q)) = sqrt ( a**2 + b**2+c**2+ d**2)
-// To Normalise a Quaternion divide each of a,b,c and d by the above value, this will make || q || = 1.
+// returns a normalized Quaternion with a length of 1
+// divide each of a,b,c and d by the length of the vector, this will make || q || = 1.
 func (q Quaternion) Norm() Quaternion {
 	mag := q.Magnitude()
-	rval := Quaternion{}
-	rval.a = q.a / mag
-	rval.b = q.b / mag
-	rval.c = q.c / mag
-	rval.d = q.d / mag
+	rval := Quaternion{q.a / mag, q.b / mag, q.c / mag, q.d / mag}
 	return rval
 }
 
@@ -35,17 +38,42 @@ func (q Quaternion) Norm() Quaternion {
 func (q Quaternion) Conj() Quaternion {
 	rval := Quaternion{}
 	rval.a = q.a
-	rval.b = -q.b
-	rval.c = -q.c
-	rval.d = q.d
+	if rval.b != -0.0 {
+		rval.b = -q.b
+	}
+	if rval.c != -0.0 {
+		rval.c = -q.c
+	}
+	if rval.d != -0.0 {
+		rval.d = -q.d
+	}
 	return rval
 }
-func Sum(a1, a2 Quaternion) Quaternion {
+
+// non-commutatve product of any number of q's
+func Prod(ain ...Quaternion) Quaternion {
+	rval := Quaternion{1, 0, 0, 0}
+	var w, x, y, z float64
+	for _, q := range ain {
+		fmt.Printf("pl: %s\n", q)
+		w = rval.a*q.a - rval.b*q.b - rval.c*q.c - rval.d*q.d
+		x = rval.a*q.b + rval.b*q.a + rval.c*q.d - rval.d*q.c
+		y = rval.a*q.c + rval.c*q.a + rval.d*q.b - rval.a*q.d
+		z = rval.a*q.d + rval.d*q.a + rval.b*q.c - rval.c*q.b
+		rval = Quaternion{w, x, y, z}
+	}
+	return rval
+}
+
+// som of any number of q's
+func Sum(ain ...Quaternion) Quaternion {
 	rval := Quaternion{}
-	rval.a = a1.a + a2.a
-	rval.b = a1.b + a2.b
-	rval.a = a1.c + a2.c
-	rval.a = a1.d + a2.d
+	for _, q := range ain {
+		rval.a += q.a
+		rval.b += q.b
+		rval.c += q.c
+		rval.d += q.d
+	}
 	return rval
 
 }
--- src/github.com/pfarrell51/cmd/quaternion_test.go
+++ src/github.com/pfarrell51/cmd/quaternion_test.go
@@ -11,8 +11,16 @@ var (
 	q1 = Quaternion{1, 0, 0, 0}
 	q2 = Quaternion{a: 10}
 	q3 = Quaternion{11, 0, 0, 0}
+	q4 = Quaternion{10, 0, 0, 0}
 )
 
+func TestNegZero(t *testing.T) {
+	var foo float64
+	foo = 0
+	fmt.Printf("pos %18.8e\n", foo)
+	foo = -foo
+	fmt.Printf("neg %18.8e\n", foo)
+}
 func TestMagnitude(t *testing.T) {
 	mag := q1.Magnitude()
 	fmt.Printf("mag: %g\n", mag)
@@ -30,13 +38,24 @@ func TestNorm(t *testing.T) {
 }
 func TestConj(t *testing.T) {
 	rval := q1.Conj()
-	fmt.Printf("%g %g %g %g\n", rval.a, rval.b, rval.c, rval.d)
+	fmt.Printf("conj: %s\n", rval)
 }
 func TestSum1(t *testing.T) {
 	rval := Sum(q1, q2)
-	fmt.Printf("in Sum1() %g %g %g %g\n", rval.a, rval.b, rval.c, rval.d)
+	fmt.Printf("in Sum1() %s \n", rval)
 	if rval != q3 {
 		t.Errorf("sum1")
+	}
+}
+func TestProd1(t *testing.T) {
+	rval := Prod(q1, q2)
+	fmt.Printf("in Prod1() %s \n", rval)
+	if rval != q4 {
+		t.Error("prod1a")
+	}
+	rval = Prod(q1, q1)
+	if rval != q1 {
+		t.Error("prod1b")
 	}
 
 }
