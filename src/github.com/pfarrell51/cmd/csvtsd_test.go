79e3f1833a6d97f0123762d0e5ece694fb09f130
Pat Farrell <pat22043@gmail.com>
2019-04-20 01:24:40 -0400

    add more test cases, all still fail

--- src/github.com/pfarrell51/cmd/csvtsd_test.go
+++ src/github.com/pfarrell51/cmd/csvtsd_test.go
@@ -11,14 +11,23 @@ var (
 	pat      = Latlong{40.064587, -75.37118}
 	doug     = Latlong{35.151035, -81.0029}
 	pittrace = Latlong{40.844715, -80.32963}
+	lyric    = Latlong{37.230427, -80.41568}
 )
 
 func TestDistance(t *testing.T) {
 	rval := pat.distance(doug)
 	fmt.Printf("to doug %g\n ", rval)
-	if rval < 5900.0 {
-		t.Fail()
+	if rval < 500 || rval > 1000.0 {
+		t.Errorf("doug %g", rval)
 	}
 	rval = pat.distance(pittrace)
 	fmt.Printf("to Pitt %g\n", rval)
+	if rval < 200.0 || rval > 300.0 {
+		t.Errorf("pitts %g", rval)
+	}
+	rval = pat.distance(lyric)
+	fmt.Printf("to Lyric %g\n", rval)
+	if rval < 400.0 || rval > 500.0 {
+		t.Errorf("Lyric %g", rval)
+	}
 }
