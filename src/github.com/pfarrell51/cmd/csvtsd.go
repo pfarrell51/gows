ab5e6b003e5a785216dcf50efe16d2811394602a
Pat Farrell <pat22043@gmail.com>
2019-04-20 19:38:07 -0400

    fix distance calculations

--- src/github.com/pfarrell51/cmd/csvtsd.go
+++ src/github.com/pfarrell51/cmd/csvtsd.go
@@ -19,14 +19,14 @@ import (
 const meterTo1kFeet = 3280
 const earthRadiusMiles = 3959
 
-type Latlong struct {
+type latlong struct {
 	lat, lng float64
 }
 
 func main() {
 	var oldTime time.Time
-	var oldlatlng Latlong
-	var oldelev int32
+//	var oldlatlng latlong
+//	var oldelev int32
 	fmt.Printf("#time, lat, long, ele, spd, bearing\n")
 	ln := 0
 	r := csv.NewReader(os.Stdin)
@@ -44,44 +44,47 @@ func main() {
 		printCSV(time, latlng, elev, 0)
 		// do stuff
 		deltaTime := time.Sub(oldTime)
-		fmt.Println(deltaTime)
+		fmt.Printf("delta time: %d\n", deltaTime)
 
 		oldTime = time
-		oldlatlng = latlng
-		oldelev = elev
+//		oldlatlng = latlng
+//		oldelev = elev
 	}
-	printCSV(oldTime, oldlatlng, oldelev, 0)
+//	printCSV(oldTime, oldlatlng, oldelev, 0)
 }
 
 // parse out current line's values, converting to useful values
-func getVals(line []string) (rtime time.Time, rlatlng Latlong, rele int32) {
-	var rlatlong = Latlong{0.0, 0.0}
+func getVals(line []string) (rtime time.Time, rlatlng latlong, rele int32) {
+	var rlatlong = latlong{0.0, 0.0}
 	rtime, err := time.Parse(time.RFC3339, strings.Trim(line[0], " "))
 	if err != nil {
 		log.Fatal(err)
 		return rtime, rlatlng, 0
 	}
 	rlatlong.lat, err = strconv.ParseFloat(strings.Trim(line[1], " 	"), 64)
-	rlatlong.lat *= (math.Pi / 180.0)
 	rlatlong.lng, err = strconv.ParseFloat(strings.Trim(line[2], " 	"), 64)
-	rlatlong.lng *= (math.Pi / 180.0)
 	relev, err := strconv.ParseInt(strings.Trim(line[3], " 	"), 0, 32)
 	relev *= meterTo1kFeet
 	relev /= 1000 // remove 1000 back to feet
 	fmt.Printf("%v %g %g %d\n", rtime, rlatlong.lat, rlatlong.lng, relev)
 	return rtime, rlatlong, rele
 }
-func printCSV(tval time.Time, latlng Latlong, ele, spd int32) {
+func printCSV(tval time.Time, latlng latlong, ele, spd int32) {
 	fmt.Printf("%v, %g, %g, %d, %d\n", tval, latlng.lat, latlng.lng, ele, spd)
 }
 
 // distance using law of cosines
 //    d = acos( sin φ1 * sin φ2 + cos φ1 * cos φ2 * cos Δλ ) * R
-func (l1 Latlong) distance(l2 Latlong) float64 {
-	var deltaLong = l1.lng - l2.lng
-	deltaLat := math.Abs(l1.lat - l2.lat)
+// note, lat long incoming is in degrees, we want radians
+func (l1 latlong) distance(l2 latlong) float64 {
+	lr1 := latlong{l1.lat * (math.Pi / 180.0), l1.lng * (math.Pi / 180.0)}
+	lr2 := latlong{l2.lat * (math.Pi / 180.0), l2.lng * (math.Pi / 180.0)}
+	fmt.Printf("1 deg (%g, %g) rads (%g, %g)\n", l1.lat, l1.lng, lr1.lat, lr1.lng)
+	fmt.Printf("2 deg (%g, %g) rads (%g, %g)\n", l2.lat, l2.lng, lr2.lat, lr2.lng)
+	deltaLong := (lr1.lng - lr2.lng)
+	deltaLat := math.Abs(lr1.lat - lr2.lat)
 	fmt.Printf("d lat: %g  dLong: %g\n", deltaLat, deltaLong)
-	partial := math.Sin(l1.lat)*math.Sin(l2.lat) + math.Cos(l1.lat)*math.Cos(l2.lat)*math.Cos(deltaLong)
+	partial := math.Sin(lr1.lat)*math.Sin(lr2.lat) + math.Cos(lr1.lat)*math.Cos(lr2.lat)*math.Cos(deltaLong)
 	fmt.Printf("partial: %g aCos(P) %g\n", partial, math.Acos(partial))
 	var rval = math.Acos(partial) * earthRadiusMiles
 	return rval
--- src/github.com/pfarrell51/cmd/csvtsd_test.go
+++ src/github.com/pfarrell51/cmd/csvtsd_test.go
@@ -8,32 +8,55 @@ import (
 )
 
 var (
-	pat      = Latlong{0.6992589566, -1.315475252} // 40.064587, -75.37118
-	pocono   = Latlong{0.7167193352, -1.315476771} // 41.064993, -75.371267}
-	doug     = Latlong{0.6135012962, -1.413767309} //35.151035, -81.0029}
-	pittrace = Latlong{0.7128747588, -1.40201653}  // 40.844715, -80.32963}
-	lyric    = Latlong{0.6497935331, -1.403518386} //37.230427, -80.41568}
+	pat      = latlong{40.064587, -75.37118}
+	pocono   = latlong{41.064993, -75.371267}
+	doug     = latlong{35.151035, -81.0029}
+	pittrace = latlong{40.844715, -80.32963}
+	lyric    = latlong{37.230427, -80.41568}
 )
 
 func TestDistance(t *testing.T) {
+	fmt.Println("home to Pocono")
 	rval := pat.distance(pocono)
 	fmt.Printf("to pocono %g\n ", rval)
 	if rval < 60 || rval > 70.0 {
 		t.Errorf("pocono %g", rval)
 	}
+
+	fmt.Println("home to Doug")
 	rval = pat.distance(doug)
 	fmt.Printf("to doug %g\n ", rval)
 	if rval < 450 || rval > 500.0 {
 		t.Errorf("doug %g", rval)
 	}
+	fmt.Println("home to Pittrace")
 	rval = pat.distance(pittrace)
 	fmt.Printf("to Pitt %g\n", rval)
 	if rval < 200.0 || rval > 300.0 {
 		t.Errorf("pitts %g", rval)
 	}
+	fmt.Println("home to Lyric")
 	rval = pat.distance(lyric)
 	fmt.Printf("to Lyric %g\n", rval)
 	if rval < 300.0 || rval > 400.0 {
 		t.Errorf("Lyric %g", rval)
+	}
+	fmt.Println("Doug to Lyric")
+	rval = doug.distance(lyric)
+	fmt.Printf("doug to Lyric %g\n", rval)
+	if rval < 140.0 || rval > 200.0 {
+		t.Errorf("Doug Lyric %g", rval)
+	}
+}
+func TestGetVals(t *testing.T) {
+	fmt.Println("testing get vals/parsing")
+	record := []string{"2019-04-14T15:08:11.917Z", " 40.8505242", " -80.3463992", "334"}
+	time, latlng, elev := getVals(record)
+	printCSV(time, latlng, elev, 0)
+	if latlng.lat < 40.0 || latlng.lat > 41.0 {
+		t.Errorf("lat %g", latlng.lat)
+	}
+	if latlng.lng < -81.0 || latlng.lng > -80.0 {
+		t.Errorf("long: %g", latlng.lng)
 	}
 }
