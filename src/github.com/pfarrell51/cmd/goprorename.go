2759cfcf856b4079f4cdfe7220fb332969cb6568
Pat Farrell <pat22043@gmail.com>
2022-11-03 18:03:41 -0400

    convert to regexp

--- src/github.com/pfarrell51/cmd/goprorename.go
+++ src/github.com/pfarrell51/cmd/goprorename.go
@@ -8,30 +8,48 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func Files(fsys fs.FS, patharg string) {
-
+	comRegex, err := regexp.Compile(".(Mm)r(pP)4")
+	if err != nil {
+		fmt.Println("PIB")
+		return
+	}
 	fs.WalkDir(fsys, patharg, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
-			fmt.Println(d)
-			fmt.Println("error is ")
-			fmt.Println(err)
+			fmt.Println("Error processing", d)
+			fmt.Println("error is ", err)
+			return nil
+		}
+		if strings.HasPrefix(path, ".") {
+			return nil
+		}
+		pathAsBytes := []byte(path)
+		fmt.Println(pathAsBytes)
+		ext := comRegex.Find(pathAsBytes)
+		if ext == nil {
+			fmt.Println("regex failed ", path)
+			return nil
+		} else {
+			fmt.Println("success ", ext)
 		}
 		fmt.Println(path)
+
 		return nil
 	})
-
 }
 
 // get argument from shell, if any or default
+
 func main() {
-	pathArg := "./*.[mMpP]4"
+	pathArg := "."
 	if len(os.Args) > 1 {
 		pathArg = os.Args[1]
 	}
 	fs := os.DirFS(pathArg)
 	fmt.Println("fs from arg: ", fs)
 	Files(fs, pathArg)
-
 }
