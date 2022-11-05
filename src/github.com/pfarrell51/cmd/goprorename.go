07981eb2493f6e53f869d66218ffe1c1f8d69873
Pat Farrell <pat22043@gmail.com>
2022-11-05 18:06:48 -0400

    fixup regex that pull apart filename, and comment linking to new testing FS

--- src/github.com/pfarrell51/cmd/goprorename.go
+++ src/github.com/pfarrell51/cmd/goprorename.go
@@ -1,6 +1,8 @@
 // shell program to rename files created by a GoPro into a single, sensible
 // ordering of files so that the order is obvious for easy processing
 // by other utulities such as RaceRender
+//
+// goPro naming conventions: https://community.gopro.com/s/article/GoPro-Camera-File-Naming-Convention?language=en_US
 
 package main
 
@@ -12,30 +14,33 @@ import (
 	"strings"
 )
 
-func Files(fsys fs.FS, patharg string) {
-	comRegex, err := regexp.Compile(".(M|m)(p|P)4")
-	if err != nil {
-		fmt.Println("PIB")
-		return
-	}
+// the new testing/fstest package provides a TestFS function that checks for and
+// reports common mistakes. It also provides a simple in-memory file systemi
+// implementation, MapFS, which can be useful for testing code that accepts fs.FS implementations."
+
+func ProcessFiles(fsys fs.FS, patharg string) {
+	extRegex := regexp.MustCompile(".(M|m)(p|P)4")
+	nameRegex := regexp.MustCompile("(?s)(GX|H)(\\d{2})(\\d{4})")
 	fs.WalkDir(fsys, patharg, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
-			fmt.Println("Error processing", d)
+			fmt.Println("Error processing", d, " in ", path)
 			fmt.Println("error is ", err)
 			return nil
 		}
 		if strings.HasPrefix(path, ".") {
 			return nil
 		}
-		pathAsBytes := []byte(path)
-		ext := comRegex.Find(pathAsBytes)
-		if ext == nil {
+		ext := extRegex.FindString(path)
+		if len(ext) == 0 {
 			return nil
-		} else {
-			fmt.Println("success ", ext)
 		}
-		fmt.Println(path)
-
+		nameParts := nameRegex.FindAllStringSubmatch(path, -1)
+		if len(nameParts) > 0 && len(nameParts[0]) > 3 {
+			prefix := nameParts[0][1]
+			chapter := nameParts[0][2]
+			clip := nameParts[0][3]
+			fmt.Println("p: ", prefix, " chpt ", chapter, " clip ", clip)
+		}
 		return nil
 	})
 }
@@ -49,5 +54,5 @@ func main() {
 	}
 	fs := os.DirFS(pathArg)
 	fmt.Println("fs from arg: ", fs)
-	Files(fs, pathArg)
+	ProcessFiles(fs, pathArg)
 }
