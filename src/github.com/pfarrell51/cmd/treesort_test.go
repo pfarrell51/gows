f7295a54f1c2f991dc88330f05b38460336fc2ce
Pat Farrell <pat22043@gmail.com>
2022-11-18 09:32:27 -0500

    add in some PMD to debug the tests

--- src/github.com/pfarrell51/cmd/treesort_test.go
+++ src/github.com/pfarrell51/cmd/treesort_test.go
@@ -4,11 +4,11 @@
 package treesort_test
 
 import (
+	"fmt"
 	"math/rand"
 	"sort"
 	"testing"
-
-	"gopl.io/ch4/treesort"
+	"treesort"
 )
 
 func TestSort(t *testing.T) {
@@ -16,7 +16,9 @@ func TestSort(t *testing.T) {
 	for i := range data {
 		data[i] = rand.Int() % 50
 	}
+	fmt.Println("data loaded")
 	treesort.Sort(data)
+	fmt.Println("data sorted")
 	if !sort.IntsAreSorted(data) {
 		t.Errorf("not sorted: %v", data)
 	}
