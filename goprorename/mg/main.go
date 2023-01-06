2572db01501f650023343c15b404772714cb67be
Pat Farrell <pat22043@gmail.com>
2023-01-06 16:33:00 -0500

    make main work with revised goprorename

--- goprorename/mg/main.go
+++ goprorename/mg/main.go
@@ -5,26 +5,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/pfarrell51/gows/goprorename"
-	"io/fs"
 	"os"
 )
 
 // get argument from shell, if any or default
-
 func main() {
 	pathArg := "."
-	if false {
-		if len(os.Args) > 1 {
-			pathArg = os.Args[1]
-		}
+	if len(os.Args) > 1 {
+		pathArg = os.Args[1]
 	}
-	pathArg = "/usr/lib"
-	ok := fs.ValidPath(pathArg)
-	fmt.Println("is OK? ", ok)
-	fsr := os.DirFS(pathArg)
-	fmt.Println("fs from arg: ", fsr)
-	fmt.Println(fsr)
-	goprorename.ProcessFiles(fsr, pathArg)
+	goprorename.ProcessFiles(pathArg)
 }
