2cefb2342877435efc29ceedb4e5c7b0eca9ca59
Pat Farrell <pat22043@gmail.com>
2023-01-09 23:12:18 -0500

    improve documentation

--- goprorename/mg/main.go
+++ goprorename/mg/main.go
@@ -1,7 +1,16 @@
-// shell program to rename files created by a GoPro into a single, sensible
-// ordering of files so that the order is obvious for easy processing
-// by other utulities such as RaceRender
+/*
+shell program to rename files created by a GoPro into a single, sensible
+ordering of files so that the order is obvious for easy processing
+by other utulities such as RaceRender.
 
+accepts a directory spec as the first argument.
+If no first argument is entered by the user, the current directory (i.e. ".") is used.
+
+Outputs a serious of lines, each a shell command to preform the renaming.
+This output can be directed to a file, as any standard output.
+
+goPro naming conventions: [https://community.gopro.com/s/article/GoPro-Camera-File-Naming-Convention?language=en_US]
+*/
 package main
 
 import (
@@ -9,7 +18,7 @@ import (
 	"os"
 )
 
-// get argument from shell, if any or default
+// get argument from shell, if any or default to "."
 func main() {
 	pathArg := "."
 	if len(os.Args) > 1 {
