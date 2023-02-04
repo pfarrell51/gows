fb06833dcdd5e18c3301ba9f81392f19236e0a7c
Pat Farrell <pat22043@gmail.com>
2023-02-04 02:44:04 -0500

    move justLetter to this file, clean it up

--- musictools/structs.go
+++ musictools/structs.go
@@ -3,9 +3,11 @@
 package musictools
 
 import (
+	"bytes"
 	"fmt"
 	"regexp"
 	"strings"
+	"unicode"
 
 	"github.com/dlclark/metaphone3"
 	g "github.com/zyedidia/generic"
@@ -70,19 +72,41 @@ func init() {
 	enc.Encode("ignore this")
 	enc.MaxLength = 20
 }
+
+// takes a string and returns just the letters.
+func justLetter(a string) string {
+	buff := bytes.Buffer{}
+	for _, c := range a {
+		if unicode.IsLetter(c) {
+			buff.WriteRune(c)
+		} else if c == '_' || c == '&' || unicode.IsSpace(c) {
+			// ignore it
+		} else if c == '-' {
+			break
+		}
+	}
+	return buff.String()
+}
+
+var regAndThe = regexp.MustCompile("(?i) (and|the) ")
+
 func StandardizeArtist(art string) string {
 	if len(art) == 0 {
 		return art
 	}
 	rval := strings.TrimSpace(art)
-	if rval == "The The" {
+	if rval == "The The" { // UK post-punk band
 		return rval
 	}
 	if matched, _ := regexp.MatchString("^[A-Z]( |-|_)", rval); matched {
 		rval = rval[2:]
 	}
-	if strings.HasPrefix(rval, "The ") {
-		rval = rval[4:]
+	for j := 0; j < 4; j++ { // 4 allows the space before the keyword (and/the), as we back up
+		loc := regAndThe.FindStringIndex(rval)
+		if len(loc) < 1 {
+			break
+		}
+		rval = rval[:loc[0]] + rval[loc[1]-1:]
 	}
 	return rval
 }
@@ -94,19 +118,16 @@ func StandardizeTitle(title string) string {
 	if matched, _ := regexp.MatchString("^[A-Z]( |-|_)", rval); matched {
 		rval = rval[2:]
 	}
-	if strings.HasPrefix(rval, "The ") {
-		rval = rval[4:]
-	}
 	rval = strings.ReplaceAll(rval, "/", " ")
 	rval = strings.ReplaceAll(rval, "_", " ")
 	return rval
 }
 func EncodeTitle(s string) (string, string) {
-	prim, sec := enc.Encode(StandardizeTitle(s))
+	prim, sec := enc.Encode(justLetter(StandardizeTitle(s)))
 	return prim, sec
 }
 func EncodeArtist(s string) (string, string) {
-	prim, sec := enc.Encode(StandardizeArtist(s))
+	prim, sec := enc.Encode(justLetter(StandardizeArtist(s)))
 	return prim, sec
 }
 
@@ -117,7 +138,7 @@ func GetArtistMap() *btree.Tree[string, string] {
 }
 func LoadArtistMap() {
 	groupNames := [...]string{
-		"5th_Dimension", "ABBA", "Alice Cooper", "Alison Krauss", "Alison Krauss Union Station",
+		"5th Dimension", "ABBA", "Alice Cooper", "Alison Krauss", "Alison Krauss Union Station",
 		"Allman Brothers", "Allman Brothers Band", "Almanac Singers",
 		"Animals", "Aquarius", "Aretha Franklin", "Arlo Guthrie", "Association", "Average White Band",
 		"Band", "Basia", "Beach Boys", "Beatles", "Bee Gees", "Billy Joel", "Blind Faith",
@@ -127,26 +148,26 @@ func LoadArtistMap() {
 		"Crosby and Nash", "Crosby Stills & Nash", "Crosby Stills And Nash", "CSN&Y",
 		"Crosby Stills Nash Young", "Crosby Stills Nash & Young", "David Allan Coe",
 		"David Bowie", "David Bromberg", "Deep Purple", "Derek and the Dominos",
-		"Derek_Dominos", "Detroit Wheels",
-		"Dire_Straits", "Doc Watson", "Don McLean", "Doobie Brothers", "Doors", "Dylan",
-		"Elton_John", "Emerson, Lake & Palmer", "Emmylou_Harris", "Fifth Dimension",
-		"Fleetwood_Mac", "Genesis",
+		"Derek Dominos", "Detroit Wheels",
+		"Dire Straits", "Doc Watson", "Don McLean", "Doobie Brothers", "Doors", "Dylan",
+		"Elton John", "Emerson, Lake & Palmer", "Emmylou Harris", "Fifth Dimension",
+		"Fleetwood Mac", "Genesis",
 		"George Harrison", "Gillian Welch", "Gillian Welch Alison Krauss", "Graham Nash",
 		"Gram Parsons", "Hall and Oates", "Hall & Oates",
 		"Heart", "Isley Brothers", "Jackie Wilson", "Jackson Browne",
-		"James_Taylor", "Jefferson_Airplane", "Jethro_Tull", "Jimmy Buffett", "John_Denver",
-		"John_Hartford", "John_Starling", "Joni_Mitchell", "Judy_Collins", "Kansas",
-		"KC The Sunshine Band", "Kingston_Trio", "Led_Zeppelin", "Linda_Ronstadt",
-		"Lovin Spoonful", "Lynyrd_Skynyrd",
+		"James Taylor", "Jefferson Airplane", "Jethro Tull", "Jimmy Buffett", "John Denver",
+		"John Hartford", "John Starling", "Joni Mitchell", "Judy Collins", "Kansas",
+		"KC The Sunshine Band", "Kingston Trio", "Led Zeppelin", "Linda Ronstadt",
+		"Lovin Spoonful", "Lynyrd Skynyrd",
 		"Mamas And Papas", "Mamas & The Papas", "Maria Muldaur",
-		"Meatloaf", "Mike_Auldridge", "Mith Ryder & Detroit Wheels", "Moody Blues",
-		"Neal Young", "Neil Diamond", "New Riders of the Purple Sage", 
+		"Meatloaf", "Mike Auldridge", "Mith Ryder & Detroit Wheels", "Moody Blues",
+		"Neal Young", "Neil Diamond", "New Riders of the Purple Sage",
 		"Nitty Gritty Dirt Band", "Oates", "Original Soundtrack", "Otis Redding", "Pablo Cruise",
-		"Paul_Simon", "Pete Seeger", "Peter Paul Mary", "Rascals", "Ringo Starr",
-		"Robert Plant Alison Kraus", "Roberta Flack", "Rolling_Stones",
-		"Roy _Orbison", "Sam And Dave", "Santana", "Seals and Crofts", "Seals Croft", "Seldom Scene",
+		"Paul Simon", "Pete Seeger", "Peter Paul Mary", "Rascals", "Ringo Starr",
+		"Robert Plant Alison Kraus", "Roberta Flack", "Rolling Stones",
+		"Roy Orbison", "Sam And Dave", "Santana", "Seals and Crofts", "Seals Croft", "Seldom Scene",
 		"Shadows Of Knight", "Simon and Garfunkel", "Simon Garfunkel", "Soggy Mountain Boys", "Sonny And Cher",
-		"Spoonful", "Seals Crofts", "Steely_Dan", "Steppenwolf", "Steven_Stills",
+		"Spoonful", "Seals Crofts", "Steely Dan", "Steppenwolf", "Steven Stills",
 		"Stevie Ray Vaughan and Double Trouble", "Sting", "Sunshine Band",
 		"Three Dog Night", "TonyRice", "Traveling Wilburys", "Turtles", "Warren Zevon",
 		"Who", "Wilson Pickett", "Yes",
