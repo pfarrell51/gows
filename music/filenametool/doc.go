3f143940cddd7322d65fade485b02b83b94b8945
Pat Farrell <pat22043@gmail.com>
2023-02-11 00:45:08 -0500

    update to reflect refactoring and removal of all metatag logic from this package

--- music/filenametool/doc.go
+++ music/filenametool/doc.go
@@ -1,23 +1,23 @@
-// Package musicfntool is Pat's tools to use a song's file name to organize and manage his music library
-// Pat has about 1000 CDs collected from 1985. He still occasionally
-// buys new ones, but most of his music is from last century
+// Package filenametool is useful for files that do not contain accurate
+// mp3/Vorbis/Flac metadata tags. It reads the files name and path and
+// attempts to figure out what artist, album and song title is
+// contained in the file.
 //
 // The basic operation is to run the executable, give it a switch/flag to say what
 // you want to do, and specify a folder/directory that will be processed
 //
 // Usage of mm/mm: [flags] directory-spec
 //
-//		-a    artist map -  list artist not in source code (gpmap)
-//		-de    debug on
-//	 	-dup  try to identify duplicate songs. Note, the same song title by two different
-//	       artists does not count as a duplicate
-//	 	-fr   rename - output command to perform rename function on needed files, the -mr is
-//	         much more reliable, use Musicbrainz' Picard utility to set the metadata
-//		-j    output metadata as json
-//		-l    list - list files
-//		-mr   rename based on Metadata. output commands to rename files based on mp3 metadata
-//		-n    nogroup - list files that do not have an artist/group in the title
-//		-z    list artist names one per line
+//  -a    artist map -  list artist not in source code. The list of artists is
+//			contained in a normal text file named data/artists.txt 
+//  -de   debug on, output lots of error messages that help debugging
+//  -dup   duplicate song.  attempts to identify duplicate songs, does not work well 
+//  -h    help
+//  -j    output metadata as json
+//  -l    list - list files
+//  -n    nogroup - list files that do not have an artist/group in the title
+//  -r    rename - output rename from parsed file name
+//  -z    list artist names one per line
 //
 // default is to list files that need love.
 //
