9479cfb2f111f05807081d294f3ee33f5620f06d
Pat Farrell <pat22043@gmail.com>
2023-02-11 20:31:08 -0500

    merge in two small func from pathgames so we can kill that file

--- music/tagtool/parsePath.go
+++ music/tagtool/parsePath.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"path"
+	"path/filepath"
 	"regexp"
 	"runtime"
 	"sort"
@@ -32,9 +34,6 @@ func ProcessFiles(pathArg string) {
 }
 
 var ExtRegex = regexp.MustCompile("((M|m)(p|P)(3|4))|((F|f)(L|l)(A|a)(C|c))$")
-
-const divP = " -+" // want space for names like Led Zeppelin - Bron-Yr-Aur
-var dashRegex = regexp.MustCompile(divP)
 
 // this is the local  WalkDirFunc called by WalkDir for each file
 // pathArg is the path to the base of our walk
@@ -96,7 +95,56 @@ func WalkFiles(pathArg string) map[string]Song {
 	})
 	return songMap
 }
+// prepopulate song structure with what we can know from the little we get from the user entered pathArg
+// and the p lower path/filename.ext that we are walking through
+func (s *Song) BasicPathSetup(pathArg, p string) {
+    joined := filepath.FromSlash(path.Join(pathArg, p))
+    s.inPath = joined
+    s.inPathDescent, _ = path.Split(p) // ignore file name for now
+    s.outPathBase = pathArg
+    s.outPath = filepath.FromSlash(path.Join(pathArg, s.inPathDescent)) // start to build from here
+    s.ext = path.Ext(p)
+    if GetFlags().Debug {
+        fmt.Printf("inpath %s\n", s.inPath)
+        if s.inPathDescent != "" {
+            fmt.Printf("inpath.descent %s\n", s.inPathDescent)
+        }
+        fmt.Printf("outpath %s\n", s.outPath)
+        fmt.Printf("outpath.base %s\n", s.outPathBase)
+        fmt.Printf("ext: %s\n", s.ext)
+    }
+}
 
+// build up output path in case we want to rename the file
+func (s *Song) FixupOutputPath() {
+    if GetFlags().Debug {
+        fmt.Printf("FOP %s\n", s.outPath)
+    }
+    if s.outPath == s.inPath {
+        return
+    }
+    if s.ext == "" {
+        panic(fmt.Sprintf("PIB, extension is empty %s\n", s.outPath))
+    }
+    if !strings.Contains(s.outPath, s.Title) {
+        s.outPath = path.Join(s.outPath, s.Title)
+    }
+    if !s.artistInDirectory && s.Artist != "" && !strings.Contains(s.outPath, s.Artist) {
+        s.outPath += "; " + s.Artist
+    }
+    if !strings.Contains(s.outPath, s.ext) {
+        s.outPath = s.outPath + s.ext
+    }
+    s.outPath = filepath.FromSlash(s.outPath)
+    if GetFlags().Debug {
+        fmt.Printf("leaving FOP %s\n", s.outPath)
+    }
+    if s.outPath == s.inPath {
+        if GetFlags().Debug {
+            fmt.Printf("#structs/fxop: no change for %s\n", s.inPath)
+        }
+    }
+}
 // this is the output routine. it goes thru the map and produces output
 // appropriate for the specified flag
 func ProcessMap(pathArg string, m map[string]Song) map[string]Song {
