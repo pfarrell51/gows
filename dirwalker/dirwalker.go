addc8e1b53a11a84419878e970d6f67e53355c12
Pat Farrell <pat22043@gmail.com>
2023-02-25 22:54:43 -0500

    early version, gets argument from main.go. play around with templates

--- dirwalker/dirwalker.go
+++ dirwalker/dirwalker.go
@@ -4,15 +4,56 @@
 package dirwalker
 
 import (
+	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"text/template"
 )
 
+type FlagST struct {
+	CopyAlbumInTrackOrder bool
+	CSV                   bool
+	Debug                 bool
+}
+type GlobalVars struct {
+	pathArg    string
+	localFlags *FlagST
+}
+type DirEntry struct {
+	InPath  string
+	OutPath string
+	Cmd     string
+}
+
+// copy user set flags to a local store
+func (g *GlobalVars) SetFlagArgs(f FlagST) {
+	g.localFlags = new(FlagST)
+	g.localFlags.CopyAlbumInTrackOrder = f.CopyAlbumInTrackOrder
+	g.localFlags.CSV = f.CSV
+	g.localFlags.Debug = f.Debug
+}
+func AllocateData() *GlobalVars {
+	rval := new(GlobalVars)
+	rval.localFlags = new(FlagST)
+	if true {
+		std1 := DirEntry{"/home/Music/flac", "/home/music/mp3", "rename "}
+		tmpl, err := template.New("test").Parse("{{.Cmd}} {{.InPath}} of {{.OutPath}}")
+		if err != nil {
+			panic(err)
+		}
+		err = tmpl.Execute(os.Stdout, std1)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(os.Stdout, "\n")
+	}
+	return rval
+}
 func Files(path string) (count int) {
 	fsys := os.DirFS(path)
 	fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
-		if filepath.Ext(p) == ".go" {
+		if filepath.Ext(p) == ".flac" {
 			count++
 		}
 		return nil
