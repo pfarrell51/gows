45db4b40924612e19963d3e69fe6a6c8283fea99
Pat Farrell <pat22043@gmail.com>
2023-03-04 02:03:59 -0500

    support skipIfBreadcrumbExists

--- dirwalker/dw/main.go
+++ dirwalker/dw/main.go
@@ -29,7 +29,7 @@ func main() {
 		w := flag.CommandLine.Output() // may be os.Stderr - but not necessarily
 		usagePrint()
 		flag.PrintDefaults()
-		fmt.Fprintf(w, "default is to apply verb to in-directory-slec yeilding out-directory-spec.\n")
+		fmt.Fprintf(w, "default is to apply verb to in-directory-spec yeilding out-directory-spec.\n")
 	}
 
 	var globals = dirwalker.AllocateData()
@@ -38,22 +38,21 @@ func main() {
 	var flags = new(dirwalker.FlagST)
 	flag.BoolVar(&flags.Debug, "de", false, "debug on")
 	flag.BoolVar(&helpflag, "h", false, "help")
+	flag.BoolVar(&flags.SkipIfBreadcrumbExists, "skip", false, "skip if breadcrumb file exists")
 	flag.Parse()
 	if helpflag {
 		flag.Usage()
 		return
 	}
-
+	numArgs := len(flag.Args())
 	globals.SetFlagArgs(*flags)
 
 	verb := flag.Arg(0)
 	if !(verb == "ffmpeg" || verb == "sox") {
-		fmt.Printf("2 verb is %s\n", verb)
 		fmt.Printf("3 Verb must be either ffmpeg or sox, not %s\n", verb)
 		flag.Usage()
 		return
 	}
-	numArgs := len(flag.Args())
 	var inpathArg, outpathArg string
 	inpathArg = path.Clean(flag.Arg(1))
 	outpathArg = path.Clean(flag.Arg(2))
@@ -68,8 +67,7 @@ func main() {
 			inpathArg = path.Clean(flag.Arg(1))
 			outpathArg = "mp3"
 		case "sox":
-			inpathArg = path.Clean(flag.Arg(1))
-			outpathArg = "sox"
+			// can't do 2 args
 		default:
 			flag.Usage()
 			return
@@ -78,9 +76,21 @@ func main() {
 		inpathArg = path.Clean(flag.Arg(1))
 		outpathArg = path.Clean(flag.Arg(2))
 		ext = strings.TrimSpace(flag.Arg(3))
+		if strings.HasPrefix(inpathArg, "-") {
+			fmt.Printf("WARNING, switches must be before the verb, %s ignored\n", inpathArg)
+			flag.Usage()
+			return
+		}
+		if strings.HasPrefix(outpathArg, "-") {
+			fmt.Printf("WARNING, switches must be before the verb, %s ignored\n", outpathArg)
+			flag.Usage()
+			return
+		}
 	}
-
-	numDone := dirwalker.Files(verb, inpathArg, outpathArg, ext)
+	if globals.Flags().Debug {
+		fmt.Printf("v: %s i: %s  o: %s\n", verb, inpathArg, outpathArg)
+	}
+	numDone := globals.Files(verb, inpathArg, outpathArg, ext)
 	fmt.Printf("#%d\n", numDone)
 	duration := time.Since(start)
 	fmt.Printf("# %v\n", duration)
