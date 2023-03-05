edc43e395dc27e46b95d852acfc22925e612cd02
Pat Farrell <pat22043@gmail.com>
2023-03-05 18:09:40 -0500

    add parallel PrintCSVtoWriter and PrintJSONtoWriter funcs so we can specify a Writer for testing

--- music/tagtool/encodings.go
+++ music/tagtool/encodings.go
@@ -3,13 +3,20 @@ package tagtool
 import (
 	"encoding/csv"
 	"encoding/json"
-	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
+// generate json from song map, output to stdout
 func PrintJson(m map[string]Song) {
+	PrintJsontoWriter(os.Stdout, m)
+}
+
+// generate json from song map, output to writer
+func PrintJsontoWriter(w io.Writer, m map[string]Song) {
 	var songs []Song
 	for _, v := range m {
 		songs = append(songs, v)
@@ -18,31 +25,40 @@ func PrintJson(m map[string]Song) {
 	if err != nil {
 		log.Fatalf("JSON marshalling failed: %s", err)
 	}
-	fmt.Println(strings.ReplaceAll(string(data), "}", string("}\n")))
+	result := strings.ReplaceAll(string(data), "}", string("}\n"))
+	b := []byte(result)
+	w.Write(b)
 }
 
+// generate CSV from song map, output to stdout
 func PrintCSV(m map[string]Song) {
+	PrintCSVtoWriter(os.Stdout, m)
+}
+
+// generate CSV from song map, output to writer
+func PrintCSVtoWriter(w io.Writer, m map[string]Song) {
 	var songs [][]string
 	for _, v := range m {
 		var aSong []string
 		aSong = append(aSong, v.Artist)
 		aSong = append(aSong, v.Album)
 		aSong = append(aSong, v.Title)
+		aSong = append(aSong, v.Genre)
+		aSong = append(aSong, strconv.Itoa(v.Track))
+		aSong = append(aSong, strconv.Itoa(v.Year))
+		aSong = append(aSong, v.MBID)
 
 		songs = append(songs, aSong)
 	}
-	w := csv.NewWriter(os.Stdout)
+	cw := csv.NewWriter(w)
 
 	for _, song := range songs {
-		if err := w.Write(song); err != nil {
+		if err := cw.Write(song); err != nil {
 			log.Fatalln("error writing record to csv:", err)
 		}
 	}
-
-	// Write any buffered data to the underlying writer (standard output).
-	w.Flush()
-
-	if err := w.Error(); err != nil {
+	cw.Flush() // Write any buffered data to the underlying writer (standard output).
+	if err := cw.Error(); err != nil {
 		log.Fatal(err)
 	}
 }
