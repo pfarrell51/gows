b30fdf4d6eff3fb3e2d9873bf61e4d9c9a0b3172
Pat Farrell <pat22043@gmail.com>
2023-03-11 23:26:02 -0500

    add switched for CPU and Memory profiling

--- msync/mm/main.go
+++ msync/mm/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/pfarrell51/gows/msync"
+	"github.com/pkg/profile"
 )
 
 func usagePrint() {
@@ -34,6 +35,8 @@ func main() {
 	var flags = new(msync.FlagST)
 	flag.BoolVar(&flags.Debug, "de", false, "debug on")
 	flag.BoolVar(&helpflag, "h", false, "help")
+	flag.BoolVar(&flags.ProfileCPU, "profcpu", false, "turn on profiling for CPU")
+	flag.BoolVar(&flags.ProfileMEM, "profmem", false, "turn on profiling for memory usage")
 	flag.Parse()
 	if helpflag {
 		flag.Usage()
@@ -50,7 +53,6 @@ func main() {
 	case numArgs >= 2:
 		inpathArg = path.Clean(flag.Arg(0))
 		outpathArg = path.Clean(flag.Arg(1))
-		fmt.Printf("arguments: %s o: %s\n", inpathArg, outpathArg)
 		if strings.HasPrefix(inpathArg, "-") || strings.HasPrefix(outpathArg, "-") {
 			fmt.Printf("WARNING, switches must be before the verb, %s i%s ignored\n", inpathArg, outpathArg)
 			flag.Usage()
@@ -59,6 +61,10 @@ func main() {
 	}
 	if globals.Flags().Debug {
 		fmt.Printf("i: %s  o: %s\n", inpathArg, outpathArg)
+	}
+	if globals.Flags().ProfileCPU {
+		defer profile.Start(profile.CPUProfile, // profile.ProfilePath(os.Getenv("HOME")),
+			profile.NoShutdownHook)
 	}
 	globals.WalkDirectories(inpathArg, outpathArg)
 	duration := time.Since(start)
