9807bc89b79ac15e9fa7a827045fef56c1e858d5
Pat Farrell <pat22043@gmail.com>
2023-03-14 01:13:02 -0400

    add -v verify switch, remove PMD

--- msync/msync.go
+++ msync/msync.go
@@ -23,6 +23,7 @@ type FlagST struct {
 	Debug      bool
 	ProfileCPU bool
 	ProfileMEM bool
+	Verify     bool
 }
 type GlobalVars struct {
 	inPath     string
@@ -30,6 +31,8 @@ type GlobalVars struct {
 	walkedDir  []string
 	localFlags *FlagST
 	location   *time.Location
+	cacheFN    string
+	cacheRval  bool
 }
 
 func (g *GlobalVars) Flags() *FlagST {
@@ -42,6 +45,7 @@ func (g *GlobalVars) SetFlagArgs(f FlagST) {
 	g.localFlags.Debug = f.Debug
 	g.localFlags.ProfileCPU = f.ProfileCPU
 	g.localFlags.ProfileMEM = f.ProfileMEM
+	g.localFlags.Verify = f.Verify
 }
 func AllocateData() *GlobalVars {
 	rval := new(GlobalVars)
@@ -79,25 +83,21 @@ func (g GlobalVars) toUTC(t time.Time) string {
 	return rval
 }
 
-// these should be in the GV struct, this is a bug farm
-var cacheFN string
-var cacheRval bool
-
 func (g GlobalVars) ifExistsBreadcrumbfile(dir string) bool {
-	if dir == cacheFN {
-		return cacheRval
+	if dir == g.cacheFN {
+		return g.cacheRval
 	}
-	cacheFN = dir
+	g.cacheFN = dir
 	fpath := filepath.Join(dir, SumFN)
 
 	info, err := os.Stat(fpath) // warning for possible race condition
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			cacheRval = false
+			g.cacheRval = false
 			return false // fmt.Println("error is ErrNotExist")
 		} else {
 			fmt.Printf("can't Stat file %s got %v\n", fpath, err)
-			cacheRval = false
+			g.cacheRval = false
 			return false
 		}
 	} else {
@@ -112,13 +112,12 @@ func (g GlobalVars) ifExistsBreadcrumbfile(dir string) bool {
 		}
 		modTime := info.ModTime()
 		fmt.Printf("%s mod time %s\n", fpath, g.toUTC(modTime))
-		cacheRval = true
+		g.cacheRval = true
 		return true // breadcrumb exists
 	}
 }
 
 func (g GlobalVars) makeDirAndInfoFile(dir string) {
-	fmt.Printf("mDIF %s\n", dir)
 	err := os.MkdirAll(dir, 0777)
 	if err != nil {
 		panic(fmt.Sprintf("falled to make directory %s", dir))
@@ -141,11 +140,9 @@ func writeDirSumFile(g *GlobalVars, path string, sumLines []string, h hash.Hash)
 	info, err := os.Stat(path) // warning for possible race condition
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			cacheRval = false
 			return // fmt.Println("error is ErrNotExist")
 		} else {
 			fmt.Printf("can't Stat file %s got %v\n", path, err)
-			cacheRval = false
 			return
 		}
 	}
@@ -153,9 +150,11 @@ func writeDirSumFile(g *GlobalVars, path string, sumLines []string, h hash.Hash)
 		return
 	}
 	fpath := filepath.Join(path, SumFN)
-	file, err := os.OpenFile(fpath, os.O_RDWR|os.O_CREATE|os.O_EXCL, PermBits)
+	file, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE, PermBits)
 	if err != nil {
 		fmt.Printf("create error: %s", err)
+		file.Close()
+		return
 	}
 	defer file.Close()
 	file.Write(g.getBytestamp())
@@ -163,16 +162,16 @@ func writeDirSumFile(g *GlobalVars, path string, sumLines []string, h hash.Hash)
 		return
 	}
 	t := fmt.Sprintf("%s %d\n", path, len(sumLines))
-	fmt.Println(t)
-	fmt.Fprintf(file, t)
+	fmt.Printf("%s", t)
+	fmt.Fprintf(file, "%s", t)
 	for _, l := range sumLines {
-		t = fmt.Sprintf("%s\n", l)
-		fmt.Println(t)
-		fmt.Fprintf(file, t)
+		t = fmt.Sprintf("wDSD %s\n", l)
+		fmt.Printf("%s", t)
+		fmt.Fprintf(file, "%s", t)
 	}
-	t = fmt.Sprintf("%s\n", base64.StdEncoding.EncodeToString(h.Sum(nil)))
-	fmt.Println(t)
-	fmt.Fprintf(file, t)
+	t = fmt.Sprintf("hDir %s\n", base64.StdEncoding.EncodeToString(h.Sum(nil)))
+	fmt.Printf("%s", t)
+	fmt.Fprintf(file, "%s", t)
 }
 
 // Called from main with both input and output paths.
@@ -186,7 +185,6 @@ func (g *GlobalVars) WalkDirectories(inpath, outpath string) {
 
 // process files, walking all of 'inpath'
 func (g *GlobalVars) WalkInputDirectories(inp string) {
-	fmt.Printf("\nWID with %s\n", inp)
 	var count int
 	var oldInDir string
 	var sumLines = make([]string, 0, 10)
@@ -199,13 +197,9 @@ func (g *GlobalVars) WalkInputDirectories(inp string) {
 		}
 		joined := filepath.Clean(filepath.Join(inp, p))
 		if d.IsDir() {
-			fmt.Printf("in loop, have directory  %s\n", joined)
 			g.walkedDir = append(g.walkedDir, joined)
-
 			if joined != oldInDir {
-				fmt.Printf("#  oldIn: %s, joined: %s\n", oldInDir, joined)
 				if oldInDir != "" {
-					fmt.Printf("#1 oldIn: %s, current: %s\n", oldInDir, joined)
 					writeDirSumFile(g, oldInDir, sumLines, hDir)
 				}
 				oldInDir = joined
@@ -231,7 +225,7 @@ func (g *GlobalVars) WalkInputDirectories(inp string) {
 		}
 		result := hFile.Sum(nil)
 		fmt.Fprintf(hDir, "%x  %s\n", result, joined) // add in line to hDir (whole directory hash)
-		nameSum := fmt.Sprintf("%s %s", joined, base64.StdEncoding.EncodeToString(result))
+		nameSum := fmt.Sprintf("nameSum: %s %s", joined, base64.StdEncoding.EncodeToString(result))
 		sumLines = append(sumLines, nameSum)
 		return nil
 	})
