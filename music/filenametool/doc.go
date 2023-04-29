76f1e8a430c049a10cfe4fd6e095a1556882aa7a
Pat Farrell <pat22043@gmail.com>
2023-04-29 00:16:30 -0400

    add verify command

--- music/filenametool/doc.go
+++ music/filenametool/doc.go
@@ -6,18 +6,19 @@
 // The basic operation is to run the executable, give it a switch/flag to say what
 // you want to do, and specify a folder/directory that will be processed
 //
-// Usage of mm/mm: [flags] directory-spec
+// Usage of fn/fn: [flags] directory-spec
 //
-//  -a    artist map -  list artist not in source code. The list of artists is
-//			contained in a normal text file named data/artists.txt 
-//  -de   debug on, output lots of error messages that help debugging
-//  -dup   duplicate song.  attempts to identify duplicate songs, does not work well 
-//  -h    help
-//  -j    output metadata as json
-//  -l    list - list files
-//  -n    nogroup - list files that do not have an artist/group in the title
-//  -r    rename - output rename from parsed file name
-//  -z    list artist names one per line
+//		 -a    artist map -  list artist not in source code. The list of artists is
+//					contained in a normal text file named data/artists.txt
+//		 -de   debug on, output lots of error messages that help debugging
+//		 -dup   duplicate song.  attempts to identify duplicate songs, does not work well
+//		 -h    help
+//		 -j    output metadata as json
+//		 -l    list - list files
+//		 -n    nogroup - list files that do not have an artist/group in the title
+//		 -r    rename - output rename from parsed file name
+//	  -v	   verify file name against tags
+//		 -z    list artist names one per line
 //
 // default is to list files that need love.
 //
