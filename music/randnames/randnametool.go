a3eb41220d83ec9720c90ebfaf396e76932c351e
Pat Farrell <pat22043@gmail.com>
2023-06-14 22:13:59 -0400

    first version that  compiles and  runs

--- music/randnames/randnametool.go
+++ music/randnames/randnametool.go
@@ -5,6 +5,13 @@ import (
 	"math/rand"
 )
 
+type FlagST struct {
+	NoTags    bool
+	TwoLetter bool
+}
+
+var localFlags = new(FlagST)
+
 func foo() {
 	s := rand.NewSource(1234567)
 	r := rand.New(s)
--- music/randnames/rn/main.go
+++ music/randnames/rn/main.go
@@ -30,35 +30,22 @@ func main() {
 		fmt.Fprintf(w, "default is to list files that need love.\n")
 
 	}
-	var globals = randnames.AllocateData()
-	if globals.GetSongTree() == nil {
-		panic("main.go global songtree is nil")
-	}
 
 	var helpflag bool
 
-	var flags = new(tagtool.FlagST)
-	flag.StringVar(&flags.CpuProfile, "cpuprofile", "", "write cpu profile to `file`")
+	var flags = new(randnames.FlagST)
 	flag.BoolVar(&helpflag, "h", false, "help")
-	flag.BoolVar(&flags.JustList, "l", false, "list - list files")
 	flag.BoolVar(&flags.NoTags, "nt", false, "notags - list files that do not have any meta tags")
 	flag.BoolVar(&flags.TwoLetter, "tl", false, "two letter - list leading two letter words in titles")
-	flag.BoolVar(&flags.ZDumpArtist, "z", false, "list artist names one per line")
 	flag.Parse()
 	if helpflag {
 		flag.Usage()
 		return
 	}
 
-	globals.SetFlagArgs(*flags)
-
-	if flags.ZDumpArtist {
-		globals.DumpGptree()
-		return
-	}
 	pathArg := path.Clean(flag.Arg(0))
-	//globals.ProcessFiles(pathArg)
-	 
+	fmt.Println(pathArg)
+
 	duration := time.Since(start)
 	fmt.Printf("# %v\n", duration)
 }
