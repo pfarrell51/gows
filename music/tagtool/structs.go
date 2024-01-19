760d96f55d4fbdfbeb4f9a530f88326100f60214
Pat Farrell <pfarrell@pfarrell.com>
2024-01-19 04:50:16 -0500

    remove song structure

--- music/tagtool/structs.go
+++ music/tagtool/structs.go
@@ -12,27 +12,6 @@ import (
 	"unicode"
 )
 
-type Song struct {
-	Artist            string
-	Album             string
-	Title             string
-	smapKey           string
-	Genre             string
-	Disc, DiscCount   int
-	Track             int    `json:",omitempty"`
-	Year              int    `json:",omitempty"`
-	ISRC              string `json:",omitempty"` // International Standard Recording Code
-	MBID              string `json:",omitempty"` // musicbrainz ID
-	AcoustID          string `json:",omitempty"` // Acoust ID
-	alreadyNew        bool
-	artistInDirectory bool
-	artistKnown       bool
-	InPath            string
-	inPathDescent     string // any descent below the pathArg aka outPathBase
-	outPath           string
-	outPathBase       string // copied from pathArg entered by the user
-	ext               string
-}
 type FlagST struct {
 	CompareTagsToTitle    bool
 	CopyAlbumInTrackOrder bool
@@ -146,7 +125,6 @@ func StandardizeTitle(title string) string {
 	return rval
 }
 
-const NAllocateSongs = 10 * 1000
 
 func AllocateData() *GlobalVars {
 	rval := new(GlobalVars)
