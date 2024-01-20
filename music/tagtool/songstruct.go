8f205383cdcb02785cc92c9fc2f02cd2ba1c5dc0
Pat Farrell <pfarrell@pfarrell.com>
2024-01-20 04:46:06 -0500

    structure for song and related common structs

---
+++ music/tagtool/songstruct.go
@@ -0,0 +1,29 @@
+// struct for Song 
+
+package tagtool
+
+
+type Song struct {
+	Artist            string
+	Album             string
+	Title             string
+	smapKey           string
+	Genre             string
+	Disc, DiscCount   int
+	Track             int    `json:",omitempty"`
+	Year              int    `json:",omitempty"`
+	ISRC              string `json:",omitempty"` // International Standard Recording Code
+	MBID              string `json:",omitempty"` // musicbrainz ID
+	AcoustID          string `json:",omitempty"` // Acoust ID
+	alreadyNew        bool
+	artistInDirectory bool
+	artistKnown       bool
+	InPath            string
+	inPathDescent     string // any descent below the pathArg aka outPathBase
+	outPath           string
+	outPathBase       string // copied from pathArg entered by the user
+	ext               string
+}
+
+const NAllocateSongs = 10 * 1000
+
