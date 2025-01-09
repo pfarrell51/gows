b3e38aa6825d9149a4e41b02a5d4a316c37d1333
Pat Farrell <pfarrell@pfarrell.com>
2025-01-08 23:50:42 -0500

    add translation table and logic to use it to replace high Unicode with ASCII

--- music/fixUnicodeTags/main.go
+++ music/fixUnicodeTags/main.go
@@ -1,13 +1,18 @@
 // this program reads MP3 and flac files
-// and writes out commands to fix unicode characters in the
+// and writes out commands to fix(remove or replace) unicode characters in the
 // artist, title and album metadata tags
 //
 // this is not multi-processing safe
+// this is not general, it uses a specific look up table rather
+// than the official "PRECIS"
+// (Preparation, Enforcement, and Comparison of Internationalized Strings in Application Protocols)
+// and is documented in RFC7564.
+//
+// for example Joshua Bell's music often uses Cyrillic or Polish, which this does not handle.
 
 package main
 
 import (
-	_ "embed"
 	"fmt"
 	"github.com/dhowden/tag"
 	"io/fs"
@@ -15,6 +20,7 @@ import (
 	"path"
 	"regexp"
 	"strings"
+	"unicode"
 )
 
 type song struct {
@@ -25,11 +31,38 @@ type song struct {
 }
 
 var repuni = map[string]string{
+	"à":      "a",
+	"á":      "a",
+	"ã":      "a",
+	"ç":      "c",
 	"é":      "e",
+	"è":      "e",
+	"ë":      "e",
+	"ì":      "i",
+	"í":      "i",
+	"ñ":      "n",
+	"ò":      "o",
 	"ó":      "o",
+	"ô":      "o",
+	"ö":      "o",
 	"ś":      "s",
+	"ù":      "u",
+	"ú":      "u",
+	"È":      "E",
 	"É":      "E",
+	"Ù":      "U",
+	"Ú":      "U",
+	"’":      "'",
+	"´":      "'",
+	"`":      "'",
+	"“":      "\"",
+	"”":      "\"",
+	"«":      "\"",
+	"»":      "\"",
 	"…":      "",
+	"⁄":      " ",
+	"\u00A0": " ", // non-breaking space
+	"\u2010": "-", // hyphen
 	"\u2013": "-", //En dash
 	"\u2014": "-", //Em dash
 	"\u2015": "―", // Horizontal bar
@@ -77,7 +110,7 @@ func processFile(pathArg string, fsys fs.FS, p string, d fs.DirEntry, err error)
 	aSong := new(song)
 	aSong.path = path.Join(pathArg, p)
 	getMetadata(aSong)
-	replaceUnicode(aSong)
+	locateUnicode(aSong)
 	printFixCommand(aSong)
 }
 func getMetadata(sn *song) *song {
@@ -111,15 +144,45 @@ func getMetadata(sn *song) *song {
 	return sn
 }
 
-func locateUnicode(aSong *song) {
+func locateUnicode(sn *song) {
+	//	fmt.Printf("working on %s\n", sn.title)
 	sn.title = replaceUnicode(sn.title)
 	sn.artist = replaceUnicode(sn.artist)
 	sn.album = replaceUnicode(sn.album)
 }
 func replaceUnicode(s string) string {
-	return ""
-}
+	var sb, ub strings.Builder
+	for _, runeValue := range s {
+		if runeValue > unicode.MaxASCII { // Check if the rune is not an ASCII character
+			fmt.Printf("%c (U+%04X)\n", runeValue, runeValue)
+			ub.WriteRune(runeValue)
+		} else {
+			if ub.Len() == 0 {
+				sb.WriteRune(runeValue)
+			} else {
+				replace(&sb, &ub)
+			}
+		}
+		if ub.Len() > 0 {
+			replace(&sb, &ub)
+		}
+	}
 
+	//fmt.Printf("processed %s\n", sb.String())
+	return sb.String()
+}
+func replace(sb, ub *strings.Builder) string {
+	// lookup
+	k := ub.String()
+	v, ok := repuni[k]
+	if !ok {
+		fmt.Printf("**** error: lookup failed for %s\n", k)
+	}
+	//fmt.Printf("k: %s f?:%t v: %s (U+%04X)\n", k, ok, v, v)
+	sb.WriteString(v)
+	ub.Reset()
+	return v
+}
 func printFixCommand(aSong *song) {
-	fmt.Printf("%s %s %s\n", aSong.title, aSong.artist, aSong.path)
+	fmt.Printf("t:%s al:%s sr: %s p: %s\n", aSong.title, aSong.album, aSong.artist, aSong.path)
 }
