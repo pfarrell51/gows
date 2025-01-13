05998cd0482d8a11432ead29ad628cc15d18963e
Pat Farrell <pfarrell@pfarrell.com>
2025-01-12 23:08:10 -0500

    add tests forRemovePunct() function

--- music/util/cleaner_test.go
+++ music/util/cleaner_test.go
@@ -1,3 +1,4 @@
+// test driver for util package
 package util
 
 import (
@@ -5,20 +6,49 @@ import (
 	"testing"
 )
 
+type testVal struct {
+	in           string
+	out          string
+	expectChange bool
+}
+
 func TestCleanUni(t *testing.T) {
-	testNames := [][]string{
-		{"Deja Vu", "Deja Vu"},
-		{"El salÃ³n MÃ©xico", "El Salon Mexico"},
-		{"Antonín Dvořák", "Antonin Dvorak"},
-		{"AntonÃ­n DvoÅ™Ã¡k", "Antonin Dvorak"},
+	//return
+	testVals := []testVal{
+		{"Déjà Vu", "Deja Vu", true},
+		{"Deja Vu", "Deja Vu", false},
+		{"Déjà Vu", "Deja Vu", true},
+		{"Eeye BÃ©", "Eeye Be", true},
+		{"El salÃ³n MÃ©xico", "El Salon Mexico", true},
+		{"Antonín Dvořák", "Antonin Dvorak", true},
+		{"AntonÃ­n DvoÅ™Ã¡k", "Antonin Dvorak", true},
 	}
 	var done bool
-	for i, v := range testNames {
-		fmt.Printf("i: %d l(v) %d v: %s\n", i, len(v), v)
-		rval := CleanUni(v[0], &done)
-		fmt.Printf("rvl: %s v0: %s  V1: %s\n", rval, v[0], v[1])
-		if rval != v[1] {
-			t.Errorf("not equal ")
+	for _, v := range testVals {
+		rval := CleanUni(v.in, &done)
+		fmt.Printf("rvl: %s d:%t v0: %s  V1: %s %t\n", rval, done, v.in, v.out, v.expectChange)
+		if rval != v.out {
+			t.Errorf("not equal r: %s != %s", rval, v.in)
+		}
+		if done != v.expectChange {
+			t.Errorf("change flag %s not as expected  %t != %t", v.in, v.expectChange, done)
+		}
+	}
+}
+func TestRemovePunct(t *testing.T) {
+	testVals := []testVal{
+		{"lovin'", "lovin", true},
+		{"don't", "dont", true},
+		{"can't", "cant", true},
+		{"I'm a' fixn' to go", "Im a fixn to go", true},
+	}
+	for _, v := range testVals {
+		rval, done := RemovePunct(v.in)
+		if rval != v.out {
+			t.Errorf("not equal %s", v.in)
+		}
+		if done != v.expectChange {
+			t.Errorf("change flag on %s not as expected  %t != %t", v.in, v.expectChange, done)
 		}
 	}
 }
