faeb545b945b4cdb9825455518f5575884d7b8ab
Pat Farrell <pfarrell@pfarrell.com>
2025-01-18 23:31:39 -0500

    pretty standard bit vector with added FirstRun to find a run of TRUE bits

---
+++ music/util/bitvector_test.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+//	"fmt"
+	"testing"
+)
+
+const bvSize = 20
+
+func TestSize(t *testing.T) {
+	var fiveHundredBits = NewBitVector(bvSize)
+	l := len(fiveHundredBits.store)
+	if l != (bvSize+63)/64 {
+		t.Errorf("not equal %d, got %d", (bvSize+63)/64, l)
+	}
+	m := fiveHundredBits.max
+	if m != bvSize {
+		t.Errorf("not equal %d, got %d", bvSize, m)
+	}
+}
+func TestRun(t *testing.T) {
+	var fHB = NewBitVector(bvSize)
+	fHB.Set(3)
+	fHB.Set(8)
+	fHB.Set(9)
+	fHB.Set(13)
+	fHB.Set(14)
+	fHB.Set(15)
+	fHB.Set(16)
+	fr := fHB.FirstRun(-1)
+	if len(fr) != 2 {
+		t.Errorf("wrong length returned: %d", len(fr))
+	}
+	if fr[0] != 8 || fr[1] != 9 {
+		t.Errorf("wrong run1 position returned: %v", fr)
+	}
+
+	skip := fr[1]
+	fr = fHB.FirstRun(bvSize * 2)
+	if len(fr) > 1 {
+		t.Errorf("for bad index, wrong length returned: %d", len(fr))
+	}
+
+	fr = fHB.FirstRun(skip)
+
+	if len(fr) != 2 {
+		t.Errorf("wrong length returned: %d", len(fr))
+		return
+	}
+	if fr[0] != 13 || fr[1] != 16 {
+		t.Errorf("wrong run2 position returned: %v", fr)
+	}
+}
