fe0c4c71f1b19b024c6d370592a01b4f9ae3a657
Pat Farrell <pfarrell@pfarrell.com>
2025-02-28 02:47:22 -0500

    add test AnyOn()

--- music/util/bitvector_test.go
+++ music/util/bitvector_test.go
@@ -7,6 +7,8 @@ import (
 
 const bvSize = 26
 
+var turnOn = []int{3, 8, 9, 13, 14, 15, 16, 20}
+
 func TestSize(t *testing.T) {
 	var fiveHundredBits = NewBitVector(bvSize)
 	l := len(fiveHundredBits.store)
@@ -18,35 +20,46 @@ func TestSize(t *testing.T) {
 		t.Errorf("not equal %d, got %d", bvSize, m)
 	}
 }
-
+func makeTestBV() BitVector {
+	//const	turnOn := []int{3, 8, 9, 13, 14, 15, 16, 20}
+	var bv = NewBitVector(bvSize)
+	for i := 0; i < len(turnOn); i++ {
+		bv.Set(turnOn[i])
+	}
+	return bv
+}
+func TestSetClear(t *testing.T) {
+	var bv = NewBitVector(bvSize)
+	bv.Set(3)
+	bv.Set(8)
+	bv.Set(9)
+	fmt.Printf("c: %b %x\n", bv.store, bv.store)
+	bv.Clear(8)
+	fmt.Printf("c: %b %x\n", bv.store, bv.store)
+}
 func TestAllTrue(t *testing.T) {
-	var fHB = NewBitVector(bvSize)
-	fHB.Set(3)
-	fHB.Set(8)
-	fHB.Set(9)
-	fHB.Set(13)
-	fHB.Set(14)
-	fHB.Set(15)
-	fHB.Set(16)
-	fHB.Set(20)
+	var banana = NewBitVector(6)
+	banana.Set(1)
+	banana.Set(3)
+	banana.Set(5)
+	ib := banana.LogicalInvert()
+	fmt.Printf("b: %x  ib: %x\n", banana.store, ib.store)
+
+	br := banana.AllTrue()
+	fmt.Printf("br: %v\n", br)
+
+	var fHB = makeTestBV()
 	fr := fHB.AllTrue()
 	fmt.Printf("fr: %v\n", fr)
 	if len(fr) != 4 {
 		t.Errorf("wrong length returned: %d", len(fr))
 	}
-	if fr[1][0] != 8 || fr[1][1] != 9 {
+	if fr[1][0] != 8 || fr[1][1] != 10 {
 		t.Errorf("wrong run1 position returned: %v", fr)
 	}
 }
 func TestRun(t *testing.T) {
-	var fHB = NewBitVector(bvSize)
-	fHB.Set(3)
-	fHB.Set(8)
-	fHB.Set(9)
-	fHB.Set(13)
-	fHB.Set(14)
-	fHB.Set(15)
-	fHB.Set(16)
+	var fHB = makeTestBV()
 	fr := fHB.FirstRun(-1)
 	if len(fr) != 2 {
 		t.Errorf("wrong length returned: %d", len(fr))
@@ -69,5 +82,22 @@ func TestRun(t *testing.T) {
 	}
 	if fr[0] != 13 || fr[1] != 16 {
 		t.Errorf("wrong run2 position returned: %v", fr)
+	}
+}
+func TestOn(t *testing.T) {
+	var fHB = makeTestBV()
+	var any = fHB.AnyOn()
+	if !any {
+		t.Errorf("wrong AnyOn: %v", any)
+	}
+}
+func TestInvert(t *testing.T) {
+	var fHB = makeTestBV()
+	var inv = fHB.LogicalInvert()
+	if fHB.Get(0) {
+		t.Errorf("inv wrong for 0")
+	}
+	if inv.Get(0) {
+		t.Errorf("wrong inv0")
 	}
 }
